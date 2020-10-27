2eb14621daf78b5bc1556cd9fe0b7e68b887d222
Luis Alberto Ruiz Sarmiento <lars5360@gmail.com>
2020-10-27 17:24:44 -0500

    Add test to view footer title

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -3,8 +3,11 @@ import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
+  const footer = mount(<Footer />);
   test('Rendere del componente Footer', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
+  });
+  test('Render titulo', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
   });
 });
