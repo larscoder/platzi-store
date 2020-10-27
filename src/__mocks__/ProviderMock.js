798e3423a89e26410f885d125e7ff98530f66c60
Luis Alberto Ruiz Sarmiento <lars5360@gmail.com>
2020-10-27 18:34:13 -0500

    Add header test

--- src/__mocks__/ProviderMock.js
+++ src/__mocks__/ProviderMock.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStore } from 'redux';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createBrowserHistory } from  'history';
+import { createBrowserHistory } from 'history';
 import initialState from '../initialState';
 import reducer from '../reducers';
 
@@ -10,11 +10,13 @@ const store = createStore(reducer, initialState);
 const history = createBrowserHistory();
 
 const ProviderMock = (props) => {
-  <Provider store={store}>
-    <Router history={history}>
-      {props.children}
-    </Router>
-  </Provider>
+  return (
+    <Provider store={store}>
+      <Router history={history}>
+        {props.children}
+      </Router>
+    </Provider>
+  );
 };
 
 export default ProviderMock;
