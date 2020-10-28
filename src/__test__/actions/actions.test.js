6b5739601d1ad1d043e990db27b45758e949d211
Luis Alberto Ruiz Sarmiento <lars5360@gmail.com>
2020-10-27 19:45:40 -0500

    Add test to removeFromCart action

--- src/__test__/actions/actions.test.js
+++ src/__test__/actions/actions.test.js
@@ -10,4 +10,13 @@ describe('Actions', () => {
     };
     expect(actions.addToCart(payload)).toEqual(expected);
   });
+
+  test('removeFromCart action', () => {
+    const payload = ProductMock;
+    const expected = {
+      type: 'REMOVE_FROM_CART',
+      payload,
+    };
+    expect(actions.removeFromCart(payload)).toEqual(expected);
+  });
 });
